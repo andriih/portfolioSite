3c80e7fe449fd9bfff34ebbf75ba399687f941f0
andriih <andrii.hnatyshyn@gmail.com>
2017-02-28 20:45:50 +0200

    BGC color added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 ///////ПЛАВНИЙ ПЕРЕХІД..............
 $('a[href^="#"]').bind('click.smoothscroll',function (e) {
- e.preventDefault();
- 
-var target = this.hash,
- $target = $(target);
- 
-$('html, body').stop().animate({
- 'scrollTop': $target.offset().top
- }, 500, 'swing', function () {
- window.location.hash = target;
- });
+	 e.preventDefault();
+	 
+	var target = this.hash,
+	 $target = $(target);
+	 
+			$('html, body').stop().animate({
+		 'scrollTop': $target.offset().top
+		 }, 500, 'swing', function () {
+	 window.location.hash = target;
+	 });
  });
